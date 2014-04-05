8fb9208b10906ebd7128d7f17fea344ad658c5f9
Brian Victor <github@brianhv.org>
2014-04-05 16:21:01 -0400

    Covered most of the SpliceAPI module.

--- test/suite/Heist/SpliceAPI/Tests.hs
+++ test/suite/Heist/SpliceAPI/Tests.hs
@@ -2,13 +2,13 @@ module Heist.SpliceAPI.Tests
   ( tests
   ) where
 
-import           Control.Exception (ErrorCall(..), try)
-import           Test.Framework (Test)
+import           Control.Exception              (ErrorCall(..), try)
+import           Control.Monad                  (unless)
+import qualified Data.Map                       as M
+import           Data.Monoid                    ((<>), mempty)
+import           Test.Framework                 (Test)
 import           Test.Framework.Providers.HUnit
-import qualified Test.HUnit as H
-import Control.Monad (unless)
-import Data.Monoid ((<>))
-import qualified Data.Map as M
+import qualified Test.HUnit                     as H
 
 ------------------------------------------------------------------------------
 import           Heist.SpliceAPI
@@ -20,11 +20,19 @@ import           Heist.SpliceAPI
 -- with ".." in the tag path (which doesn't currently work).
 
 tests :: [Test]
-tests = [ testCase     "spliceapi/keyExistsError"       keyExistsErrorTest
-        , testCase     "spliceapi/unionThrowsError"     unionThrowsErrorTest
+tests = [ testCase     "spliceapi/keyExistsError"           keyExistsErrorTest
+        , testCase     "spliceapi/overwrite"                overwriteTest
+        , testCase     "spliceapi/keepExisting"             keepExistingTest
+        , testCase     "spliceapi/noDuplicateThrowsNoError" noDuplicateThrowsNoErrorTest
+        , testCase     "spliceapi/namespace"                namespaceTest
+        , testCase     "spliceapi/namespaceWithNull"        namespaceWithNullTest
+        , testCase     "spliceapi/splicesToList"            splicesToListTest
+        , testCase     "spliceapi/monoid"                   monoidTest
+        , testCase     "spliceapi/applyS"                   applySTest
+        , testCase     "spliceapi/insertS"                  insertSTest
         ]
 
-{- | Asserts that a specific exception is raised by a given action. -}
+{- | Asserts that an error is raised by a given action. -}
 assertRaisesError :: (Show a) =>
                 String -> String -> IO a -> IO ()
 assertRaisesError description expectedMessage action = do
@@ -42,17 +50,28 @@ assertRaisesError description expectedMessage action = do
 
 -- Makes sure the splices get evaluated fully enough to cause errors to get thrown
 forceSpliceEval :: Splices String -> IO ()
-forceSpliceEval ss = if (M.lookup "asdf" $! runSplices ss) /= Just "asdf" then return () else return ()
+forceSpliceEval ss = if length (show $ runSplices ss) > 0 then return () else return ()
 
 keyExistsErrorTest :: IO ()
 keyExistsErrorTest =
     assertRaisesError "Adding duplicate key raises error"
-                      "Key \"asdf\" already exists in the splice map" $ forceSpliceEval mySplices
+                      "Key \"asdf\" already exists in the splice map" $ forceSpliceEval splices
     where
-    mySplices :: Splices String
-    mySplices = do
+    splices :: Splices String
+    splices = do
         "asdf" #! "first"
         "asdf" #! "second"
+
+noDuplicateThrowsNoErrorTest :: IO ()
+noDuplicateThrowsNoErrorTest =
+    H.assertEqual "Not adding duplicate key throws no error"
+                  (Just "asdfvalue")
+                  (M.lookup "asdf" (runSplices splices)) where
+    splices :: Splices String
+    splices = do
+        "asdf" #! "asdfvalue"
+        "foo" #! "foovalue"
+                                        
 
 unionThrowsErrorTest :: IO ()
 unionThrowsErrorTest = assertRaisesError "Adding duplicate key with union raises error"
@@ -61,12 +80,71 @@ unionThrowsErrorTest = assertRaisesError "Adding duplicate key with union raises
     splices1, splices2 :: Splices String
     splices1 = "asdf" #! "first"
     splices2 = "asdf" #! "second"
---
---peopleTest :: IO ()
---peopleTest = do
---    res <- personListTest "templates"
---    H.assertEqual "people splice" expected res
---  where
---    expected =
---      "&#10;<p>Doe, John: 42&#32;years old</p>&#10;&#10;<p>Smith, Jane: 21&#32;years old</p>&#10;&#10;"
 
+overwriteTest :: IO ()
+overwriteTest = H.assertEqual "Adding duplicate key with ## overwrites"
+                              (Just "second")
+                              (M.lookup "asdf" (runSplices splices)) where
+    splices :: Splices String
+    splices = do
+        "asdf" ## "first"
+        "asdf" ## "second"
+    
+keepExistingTest :: IO ()
+keepExistingTest = H.assertEqual "Adding duplicate key with #? overwrites"
+                                 (Just "first")
+                                 (M.lookup "asdf" (runSplices splices)) where
+    splices :: Splices String
+    splices = do
+        "asdf" #? "first"
+        "asdf" #? "second"
+
+namespaceTest :: IO ()
+namespaceTest = H.assertEqual "Namespacing changes keys"
+                              (Just "foovalue")
+                              (M.lookup "ns:foo" (runSplices (namespaceSplices "ns" splices))) where
+    splices :: Splices String
+    splices = do
+        "foo" ## "foovalue"
+        "bar" ## "barvalue"
+
+namespaceWithNullTest :: IO ()
+namespaceWithNullTest = H.assertEqual "Namespacing uses only namespace if key is empty"
+                                      (Just "emptyvalue")
+                                      (M.lookup "ns" (runSplices (namespaceSplices "ns" splices))) where
+    splices :: Splices String
+    splices = do
+        "" ## "emptyvalue"
+
+splicesToListTest :: IO ()
+splicesToListTest = H.assertEqual "Splices to list works"
+                    [("bar", "barvalue"), ("foo", "foovalue")]
+                    (splicesToList splices) where
+    splices :: Splices String
+    splices = do
+        "foo" ## "foovalue"
+        "bar" ## "barvalue"
+
+monoidTest :: IO ()
+monoidTest = H.assertEqual "Monoid implementation"
+             [("foo", "foovalue")]
+             (splicesToList (mempty <> splices)) where
+    splices :: Splices String
+    splices = do
+        "foo" ## "foovalue"
+
+applySTest :: IO ()
+applySTest = H.assertEqual "applyS"
+             [("key", "appliedvalue"::String)]
+             (splicesToList $ splices $$ "applied") where
+    splices :: Splices (String -> String)
+    splices = do
+        ("key" ## (++"value"))
+
+insertSTest :: IO ()
+insertSTest = H.assertEqual "applyS"
+              [("bar", "barvalue"), ("foo", "foovalue")]
+              (splicesToList $ insertS "bar" "barvalue" splices) where
+    splices :: Splices String
+    splices = do
+        ("foo" ## "foovalue")
